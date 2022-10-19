0a2e491b353ab743fef4c4bb5560da777a39f474
peru clarice <claricekibii@gmail.com>
2022-10-19 23:41:41 +0000

    final commit

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,38 +1,4 @@
-// This is the class TryingMethods
-
-// Create a class with the name "TryingMethods"
-// Add comments to describe what this class does
-
-// Add the a main method to this class
-
-// Add at least four methods in this class
-
-// Two of the method should be dynamic and the other two static
-
-// One of the dynamic methods should have an integer as an argument.
-// It return a string of the day of the week based on the argument that
-// was provided. Use a switch case in the body of the method.
-// The second dynamic method should return nothing but just print a
-// message to the console using an if statement with at least 3 blocks.
-// The conditions of the statement should be based on the argument or
-// parameter(s) of the method.
-
-// In one of the static methods use a while loop to find the sum of all
-// numbers less than 1000 and return the result.
-
-// Lastly, in the last method use a for loop to find the sum of even
-// numbers between 12 and 103.
-
-// ...
-
-// Push your codes to github with at least 5 commits. Ensure to use at
-// least 5 branches to manage your code.
-
-// Find someone, who you will collaborate with. The collaborator will be
-// the one to accept the PRs you raise.
-
-// Thank you and happy coding.
-
+// Creating the TryingMethods class
 public class TryingMethods {
 
     // Dynamic Method 1
